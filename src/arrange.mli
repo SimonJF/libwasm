626dad1c32368a2d3fc9a92aeb9a1735b748b54d
Simon Fowler <simon@simonjf.com>
2018-05-09 11:22:34 +0100

    expose some printing functions in arrange.mli

--- src/arrange.mli
+++ src/arrange.mli
@@ -4,3 +4,10 @@ val instr : Ast.instr -> sexpr
 val func : Ast.func -> sexpr
 val module_ : Ast.module_ -> sexpr
 val script : [`Textual | `Binary] -> Script.script -> sexpr list
+
+(* String operations *)
+val unop : Ast.unop -> string
+val binop : Ast.binop -> string
+val testop : Ast.testop -> string
+val relop : Ast.relop -> string
+val cvtop : Ast.cvtop -> string
